09c302af34bc5866e04c6dc19d2a5dbfbc495af9
milanzezeljgs <milan.zezelj@greenstate.ch>
2024-02-27 09:06:37 +0100

    Improved UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_poc/models/item_model.dart';
 
@@ -16,70 +17,97 @@ class HomeScreen extends ConsumerWidget {
         title: Text("Riverpod POC"),
       ),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Expanded(
-                child: ListView.builder(
-                  itemCount: items.length,
-                  itemBuilder: (context, index) => Row(
-                    children: [
-                      Expanded(
-                        child: Row(
-                          children: [
-                            Text("Name: "),
-                            Text(items[index].name),
-                          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) => Row(
+                      children: [
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Text(
+                                "Name: ",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 15),
+                              ),
+                              Text(items[index].name),
+                            ],
+                          ),
                         ),
-                      ),
-                      Expanded(
-                        child: Row(
-                          children: [
-                            Text("Description: "),
-                            Text(items[index].description),
-                          ],
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Text(
+                                "Description: ",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 15),
+                              ),
+                              Text(
+                                items[index].description,
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextFormField(
-                      controller: _nameController,
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: _nameController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(10),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: TextFormField(
-                      controller: _descriptionController,
+                    SizedBox(width: 5),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _descriptionController,
+                        decoration: InputDecoration(
+                          contentPadding: EdgeInsets.all(10),
+                          border: OutlineInputBorder(),
+                        ),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      ref.read(itemsProvider.notifier).addItem(Item(
-                          name: _nameController.text,
-                          description: _descriptionController.text));
-                    },
-                    child: Text("Add"),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      ref
-                          .read(itemsProvider.notifier)
-                          .removeItem(_nameController.text);
-                    },
-                    child: Text("Remove"),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          ref.read(itemsProvider.notifier).addItem(Item(
+                              name: _nameController.text,
+                              description: _descriptionController.text));
+                        },
+                        child: Text("Add"),
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          ref
+                              .read(itemsProvider.notifier)
+                              .removeItem(_nameController.text);
+                        },
+                        child: Text("Remove"),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
